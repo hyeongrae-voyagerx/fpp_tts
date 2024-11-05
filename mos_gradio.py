ccb8afd0747bb8fa4c2a548123559d3586fe710e
hrim <hyeongrae.ihm@voyagerx.com>
2024-11-05 17:21:24 +0900

    edit mos_gradio

--- mos_gradio.py
+++ mos_gradio.py
@@ -2,16 +2,17 @@ import gradio as gr
 from glob import glob
 from random import shuffle
 from collections import defaultdict
+from datetime import datetime
 
-audio_list_f = glob("test/f_*.wav")
-audio_list_f.sort(key=lambda x: x[::-1])
-audio_list_m = glob("test/m_*.wav")
-audio_list_m.sort(key=lambda x: x[::-1])
+# audio_list_f = glob("test/f_*.wav")
+# audio_list_f.sort(key=lambda x: x[::-1])
+audio_list_m = glob("test1/m_*.wav")
+audio_list_m.sort(key=lambda x: x.split("_")[2])
 data = []
-for i in range(0, len(audio_list_f), 2):
-    d = [audio_list_f[i], audio_list_f[i+1]]
-    shuffle(d)
-    data.append(d)
+# for i in range(0, len(audio_list_f), 2):
+#     d = [audio_list_f[i], audio_list_f[i+1]]
+#     shuffle(d)
+#     data.append(d)
 for i in range(0, len(audio_list_m), 2):
     d = [audio_list_m[i], audio_list_m[i+1]]
     shuffle(d)
@@ -22,15 +23,18 @@ def who_win(sample, pref):
     elif pref == 2: idx = 1
     else: return "draw"
 
-    if "baseline" in sample[idx]: return "baseline"
+    if "base" in sample[idx]: return "baseline"
     if "gen" in sample[idx]: return "gen"
 
 def create_interface(audio_list):
-    def play_audio(*prefs):
+    def report(*prefs):
         result = defaultdict(int)
         for p, d in zip(prefs, data):
             result[who_win(d, p)] += 1
-        return f"result: {dict(result)}"
+        filename = "mos_result/" + format(datetime.now()).replace(" ", "_").replace(".", "_") + ".txt"
+        with open(filename, "w") as fw:
+            fw.write(f"result: {dict(result)}")
+        return f"result: {dict(result)} / 감사합니다!"
     prefs = []
     with gr.Blocks() as demo:
         for audio_pair in audio_list:
@@ -52,8 +56,8 @@ def create_interface(audio_list):
         # 제출 버튼 클릭 시 선호도를 처리
         output = gr.Textbox(label="결과")
         submit_button = gr.Button("제출")
-        submit_button.click(play_audio, inputs=prefs, outputs=output)
+        submit_button.click(report, inputs=prefs, outputs=output)
 
-    demo.launch()
+    demo.launch(server_name="0.0.0.0")
 
 create_interface(data)
